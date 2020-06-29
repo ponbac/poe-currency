07c97967cdc5e9d2672d60581652ddc659f0f8bc
ponbac <ponbac@student.chalmers.se>
2020-06-29 15:56:00 +0000

    small update to placeholder start screen

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'UNDER CONSTRUCTION',
-          style: TextStyle(fontSize: 50),
-          textAlign: TextAlign.center,
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Container(
+        child: Center(
+          child: Text(
+            'UNDER CONSTRUCTION',
+            style: TextStyle(fontSize: 50),
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
