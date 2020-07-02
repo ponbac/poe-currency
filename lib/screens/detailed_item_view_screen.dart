04802cfee9a2e9a91cf40dfaf81a07977f7f57d6
ponbac <ponbac@student.chalmers.se>
2020-07-02 12:11:03 +0000

    improved detailed item view

--- lib/screens/detailed_item_view_screen.dart
+++ lib/screens/detailed_item_view_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:poe_currency/models/item.dart';
 
@@ -12,11 +13,39 @@ class DetailedItemViewScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('${item.descrText}'),
-            RaisedButton(
-                child: Text('Back to stash'),
-                onPressed: () => Navigator.pop(context))
+            Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 3.0),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              height: 150,
+              width: 150,
+              child: FittedBox(
+                child: CachedNetworkImage(
+                  imageUrl: item.icon,
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      Center(
+                    child: SizedBox(
+                      height: 20,
+                      width: 20,
+                      child: CircularProgressIndicator(
+                          value: downloadProgress.progress),
+                    ),
+                  ),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Text('${item.explicitMods[0]}\n${item.descrText}', textAlign: TextAlign.center,),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: RaisedButton(
+                  child: Text('Back to stash'),
+                  onPressed: () => Navigator.pop(context)),
+            )
           ],
         ),
       ),
